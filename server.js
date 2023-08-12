d9bd0df881e8f54d8c0aed8a7583b7e942a14a1f
SaadHussein <sa3dhusseein@gmail.com>
2023-08-12 23:38:51 +0300

    trying fix error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
+app.use(cors({ origin: "*" }));
 require("dotenv").config();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
